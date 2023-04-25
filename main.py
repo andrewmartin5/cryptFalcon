158000dfa2c6a6c1dc3909139161d41a29193c9e
andrewmartin5 <amlefty5@gmail.com>
2023-04-25 12:06:32 -0400

    translation files

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import datetime
 import pandas
 from binance.client import Client
+from binance.enums import *
 import schedule
 from time import sleep
 import multiprocessing
@@ -12,22 +13,30 @@ import tqdm
 
 # REF: https://python.plainenglish.io/how-to-download-trading-data-from-binance-with-python-21634af30195
 
-symbol = "ETHUSDT" # "BTCUSDT"
-url = "https://testnet.binance.vision/api"
-api_key = os.environ.get("binance_api")
-api_secret = os.environ.get("binance_secret")
+symbol = "ETHUSDT"  # "BTCUSDT"
+url = "https://www.binance.us/api"
+api_key = os.environ.get('BINANCE_API')
+api_secret = os.environ.get('BINANCE_SECRET')
 STARTING_CASH = 100
 
 data = pandas.DataFrame()
 prices = []
 
+tracked1Day = []
+tracked14Day = []
+tracked30Day = []
+tracked180Day = []
+
 # TODO update to do klines
+
+
 def scrapeRecent():
     client = Client(api_key, api_secret)
     client.API_URL = url
     price = client.get_symbol_ticker(symbol=symbol)
     del client
-    return price["price"]
+    return float(price["price"])
+
 
 def scrapeHist():
     interval = "1h"
@@ -47,7 +56,8 @@ def scrapeHist():
     for list in hist:
         dates.append(datetime.datetime.fromtimestamp(list[0] / 1000.0))
         inf.append(
-            [datetime.datetime.fromtimestamp(list[0] / 1000.0), list[1], list[5]]
+            [datetime.datetime.fromtimestamp(
+                list[0] / 1000.0), list[1], list[5]]
         )
     print("Data Processed       ")
     print("Creating CSV", end="\r")
@@ -57,10 +67,8 @@ def scrapeHist():
     raw.to_csv(symbol + ".csv", index=None, header=True)
 
     raw = getRawHist()
-
-    avg = writeAvgPrice(raw)
     del client
-    return avg
+    return raw
 
 
 def dateparse(datelist):
@@ -87,15 +95,18 @@ def getAvgHist():
         parse_dates=True,
         date_parser=dateparse,
         index_col="Time",
-        names=["Time", "Price", "Vol", "1DayAvg", "14DayAvg", "30DayAvg", "180DayAvg"],
+        names=["Time", "Price", "Vol", "1DayAvg",
+               "14DayAvg", "30DayAvg", "180DayAvg"],
         header=0,
     )
     index = df.index
     df = df[~index.duplicated(keep="first")]
     return df
 
+
 def query(data, time, value):
     return data.loc[time][value]
+
 
 def avgPrice(data, startTime, days):
     time = startTime
@@ -120,8 +131,11 @@ def writeAvgPrice(data):
     for n in range(len(timelist)):
         timelist[n] = pandas.to_datetime(timelist[n])
 
-
     print("Calculating Avgs")
+    global tracked1Day
+    global tracked14Day
+    global tracked30Day
+    global tracked180Day
     tracked1Day = []
     avg1Day = []
     tracked14Day = []
@@ -130,7 +144,6 @@ def writeAvgPrice(data):
     avg30Day = []
     tracked180Day = []
     avg180Day = []
-    prediction = []
     for time in tqdm.tqdm(timelist):
         if len(tracked1Day) > 24:
             tracked1Day.pop(0)
@@ -148,14 +161,12 @@ def writeAvgPrice(data):
         avg30Day.append(sum(tracked30Day) / len(tracked30Day))
         tracked180Day.append(query(data, time, "Price"))
         avg180Day.append(sum(tracked180Day) / len(tracked180Day))
-        prediction = avg30Day[len(avg30Day) - 1] - avg14Day[len(avg14Day) - 1]
 
     newData = data
     newData["1DayAvg"] = avg1Day
     newData["14DayAvg"] = avg14Day
     newData["30DayAvg"] = avg30Day
     newData["180DayAvg"] = avg180Day
-    # newData["Prediction"] = prediction
 
     print("Data Averaged      ")
 
@@ -164,7 +175,44 @@ def writeAvgPrice(data):
 
 
 def update():
-    print(str(pandas.Timestamp.now().round('60min').to_pydatetime()) + " " + scrapeRecent())
+    print(str(pandas.Timestamp.now().round('60min').to_pydatetime()))
+    price = scrapeRecent()
+    cryptBalance = getBalance("ETH")
+    balanceUSD = getBalance("USD")
+    global tracked1Day
+    global tracked14Day
+    global tracked30Day
+    global tracked180Day
+    if len(tracked1Day) > 24:
+        tracked1Day.pop(0)
+    if len(tracked14Day) > 336:
+        tracked14Day.pop(0)
+    if len(tracked30Day) > 720:
+        tracked30Day.pop(0)
+    if len(tracked180Day) > 4320:
+        tracked180Day.pop(0)
+    tracked1Day.append(price)
+    tracked14Day.append(price)
+    tracked30Day.append(price)
+    tracked180Day.append(price)
+    avg1Day = sum(tracked1Day) / len(tracked1Day)
+    avg14Day = sum(tracked14Day) / len(tracked14Day)
+    avg30Day = sum(tracked30Day) / len(tracked30Day)
+    avg180Day = sum(tracked180Day) / len(tracked180Day)
+    # The price is increasing if the current price is higher than the average
+    priceIncreasing = avg30Day < avg14Day and avg1Day > avg30Day
+    if priceIncreasing:
+        # If the average price is greater than the current price, buy
+        diff = balanceUSD
+        balanceUSD -= diff
+        cryptBalance += diff / price
+    elif cryptBalance * scrapeRecent() > balanceUSD:
+        # If price is decreasing, sell
+        diff = cryptBalance
+        cryptBalance -= diff
+        balanceUSD += (diff * price) * .999  # To account for 0.1% Fee
+    print(f"Balance: {balanceUSD + (price * cryptBalance)}")
+
 
 def mainloop():
     # TODO change to hour.at(:"01   ")
@@ -173,6 +221,7 @@ def mainloop():
     while True:
         schedule.run_pending()
 
+
 def findIntersections(data):
     timelist = list(data.index.values)
     timelist = [pandas.to_datetime(n) for n in timelist]
@@ -180,44 +229,68 @@ def findIntersections(data):
     balanceUSD = query(data, timelist[0], "Price")
     cryptBalance = 0
 
-    avgEarnings = (STARTING_CASH / query(data, timelist[0], "Price")) * query(data, timelist[len(timelist) - 1], "Price")
+    avgEarnings = (STARTING_CASH / query(data, timelist[0], "Price")) * query(
+        data, timelist[len(timelist) - 1], "Price")
     print(f"Final price to beat: {avgEarnings}")
 
-    df = pandas.DataFrame(index=timelist, columns=["Price", "Earnings", "Cash"])
+    df = pandas.DataFrame(index=timelist, columns=[
+                          "Price", "Earnings", "Cash"])
 
-    for time in timelist:
+    for time in tqdm.tqdm(timelist):
         price = query(data, time, "Price")
         # The price is increasing if the current price is higher than the average
         # priceIncreasing = query(data, time, "30DayAvg") < query(data, time, "1DayAvg")
-        priceIncreasing = query(data, time, "30DayAvg") < query(data, time, "14DayAvg")
-        prediction = query(data, time, "1DayAvg") > query(data, time, "30DayAvg")
+        priceIncreasing = query(data, time, "30DayAvg") < query(
+            data, time, "14DayAvg")
+        prediction = query(data, time, "1DayAvg") > query(
+            data, time, "30DayAvg")
         if priceIncreasing and prediction:
             # If the average price is greater than the current price, buy
             diff = balanceUSD
             balanceUSD -= diff
             cryptBalance += diff / price
-            lastpurchase = price
         elif not priceIncreasing:
             # If price is decreasing
             diff = cryptBalance
             cryptBalance -= diff
-            balanceUSD += (diff * price) * .999 #To account for 0.1% Fee
-            lastpurchase = price
+            balanceUSD += (diff * price) * .999  # To account for 0.1% Fee
         df["Price"][time] = price
         df["Earnings"][time] = balanceUSD + (price * cryptBalance)
         df["Cash"][time] = balanceUSD
     df.to_csv(symbol + "_EarningsTime.csv", index=timelist, header=True)
     print(balanceUSD + (price * cryptBalance))
 
+
+def getBalance(sym):
+    client = Client(api_key, api_secret)
+    client.API_URL = url
+    price = client.get_asset_balance(asset=sym)
+    del client
+    return float(price["free"])
+
+
+def buy():
+    q = round((getBalance("USD")/scrapeRecent()) * .9, 5)
+    client = Client(api_key, api_secret)
+    client.API_URL = url
+    client.create_order(symbol="ETHUSDT",
+                        side=SIDE_BUY,
+                        type=ORDER_TYPE_MARKET,
+                        quantity=q)
+
+
 def main():
     # data = scrapeHist()
-    data = getAvgHist()
+    # raw = getRawHist()
+    # avg = writeAvgPrice(raw)
+    # mainloop
+    # update()
+    buy()
+    # data = getAvgHist()
     # writeAvgPrice(data)
-    
-    findIntersections(data)
     # runLinear(data)
     # emailSelf()
-    
+
 
 if __name__ == "__main__":
     main()
